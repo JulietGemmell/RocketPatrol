569e51e5c74e60fbc3dedc00d70eeeb54ed123d6
Juliet Gemmell <jegemmel@ucsc.edu>
2022-04-12 11:05:30 -0700

    fixed movement, firing is still broken

--- Src/prefabs/Rocket.js
+++ Src/prefabs/Rocket.js
@@ -9,35 +9,33 @@ class Rocket extends Phaser.GameObjects.Sprite {
       this.moveSpeed = 2;       // pixels per frame
     }
 
-    Update (){
-        //left/right movement
-        if(!this.isFiring){
-            if(keyLEFT.isDown && this.x >= borderUISize + this.width){
+    update() {
+        // left/right movement
+        if(!this.isFiring) {
+            if(keyLEFT.isDown && this.x >= borderUISize + this.width) {
                 this.x -= this.moveSpeed;
             } else if (keyRIGHT.isDown && this.x <= game.config.width - borderUISize - this.width) {
                 this.x += this.moveSpeed;
             }
         }
-
-        //fire button
-        if(Phaser.Input.Keyboard.JustDown(keyF)) {
+        // fire button
+        if(Phaser.Input.Keyboard.JustDown(keyF) && !this.isFiring) {
             this.isFiring = true;
+            this.sfxRocket.play();
         }
-
-        //if fired, move up
+        // if fired, move up
         if(this.isFiring && this.y >= borderUISize * 3 + borderPadding) {
             this.y -= this.moveSpeed;
         }
-
-        //reset on miss
+        // reset on miss
         if(this.y <= borderUISize * 3 + borderPadding) {
-            this.isFiring = false;
-            this.y = game.config.height - borderUISize - borderPadding;
+            this.reset();
         }
     }
 
+    // reset rocket to "ground"
     reset() {
         this.isFiring = false;
-        this.y = game.config.height = borderUISize - borderPadding;
+        this.y = game.config.height - borderUISize - borderPadding;
     }
-  }
+}
